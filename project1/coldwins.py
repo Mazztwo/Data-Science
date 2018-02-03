22e7d22e41e2a847339c5488b521ac2b18a04c8e
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-02-03 10:52:54 -0500

    Reads csv, pulls names of teams and number of games they played.

--- project1/coldwins.py
+++ project1/coldwins.py
@@ -1,11 +1,7 @@
-import os
-import json
 import csv
 import sys
-from requests import get
-import matplotlib.pyplot as plt
-import pandas as pd
-import datetime
+
+
 
 
 
@@ -15,29 +11,69 @@ def main(argv):
     # Open CSV input file in read mode
     # Open CSV output file in write mode, overwrite if already exists
     csv_raw_input = open(argv[1], newline='')
-    csv_raw_output = open(argv[2], "w");
-
+    csv_raw_output = open(argv[2], "w")
 
     # Create CSV objects with raw files
     csv_file_input = csv.reader(csv_raw_input)
-    csv_file_output = csv.writer(csv_raw_output)
+    #csv_file_output = csv.writer(csv_raw_output)
 
-    headers = ["team", "AVG.TEMP", "AVG.HUM" ]
-    teams = []
+    #headers = ["team", "AVG.TEMP", "AVG.HUM" ]
+    teams = {}
+    numRow = 0
+
+
+    # dict.keys()
+    # dict.values()
+    # dict.items()
     
-
+    # team = row[1]
+    # temp = row[5]
+    # windchill = row[6]
 
     for row in csv_file_input:
 
-       if(teams.count(row[1]) < 1) 
+        if(numRow != 0):
+
+            if(row[1] in teams):
+                teams[row[1]] += 1
+            else:
+                teams[row[1]] = 1
+
+            numRow += 1
+        else:
+            numRow += 1
+
+    # Check superbowl, last game, and remove data from those
+    #   
+
+    print(teams.items())
+    print("NUM ROWS: %d" % (numRow))
 
 
 
-    //csv_file_output.writerow()
+    #csv_file_output.writerow()
 
     # Close files at end
     csv_raw_input.close() 
     csv_raw_output.close()  
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+
+
+
 
 
 
@@ -60,9 +96,3 @@ def main(argv):
             
    # print("Close CSV file.\n")
    # csv_raw.close()
-
-
-
-
-if __name__ == "__main__":
-    main(sys.argv)
