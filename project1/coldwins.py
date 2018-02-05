f20c30e42514a055137c9e4c9a50d33a07b8491c
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-02-05 10:49:27 -0500

    PROJECT COMPLETE: all specifications met.

--- project1/coldwins.py
+++ project1/coldwins.py
@@ -1,10 +1,11 @@
+# Alessio Mazzone
+# CS 1656 - Introduction to Data Science
+# Assignment #1: Python
+
 import csv
 import sys
 
-
-
 def addDecimals(num):
-
     if( len(str(num).rsplit('.')[-1]) == 2 ):
         return num
     elif( len(str(num).rsplit('.')[-1]) == 1 ):
@@ -14,12 +15,9 @@ def addDecimals(num):
         num = str(num) + ".00"
         return str(num)
     else:
-        pass
-
-
+        return str(num)
 
 def main(argv):
-  
     # Open CSV input file in read mode
     # Open CSV output file in write mode, overwrite if already exists
     csv_raw_input = open(argv[1], newline='')
@@ -28,12 +26,10 @@ def main(argv):
     # Create CSV objects with raw files
     csv_file_input = csv.reader(csv_raw_input)
     csv_file_output = csv.writer(csv_raw_output)
-
     last_row = []
 
     # Headers for output file
     output_headers = ["team_name", "AVG.TEMP", "AVG.HUM", "PER.WINS", "PER.WINS.HOME", "PER.WINS.AWAY", "PER.WINS.TEMP.LESS", "PER.WINS.TEMP.MORE", "PER.WINS.HUM.LESS", "PER.WINS.HUM.MORE"]
-
     games_played = {}
     temps = {}
     humidity = {}
@@ -46,13 +42,7 @@ def main(argv):
     per_wins_temp_more = {}
     per_wins_hum_less = {}
     per_wins_hum_more = {}
-
     numRow = 0
-
-    # dict.keys()
-    # dict.values()
-    # dict.items()
-
 
     for row in csv_file_input:
         last_row = row
@@ -126,6 +116,7 @@ def main(argv):
             numRow += 1
         else:
             numRow += 1
+    
     # Remove superbowl temp/humidity data
     if(last_row[6] != ''):
         temps[last_row[1]] -= min(int(last_row[5]), int(last_row[6]))
@@ -136,6 +127,7 @@ def main(argv):
             humidity[last_row[1]] -= int(last_row[7][0])
         else:
             humidity[last_row[1]] -= int(last_row[7][0:2])
+    
     # Calculate AVG.TEMP
     for team in temps:
 
@@ -144,6 +136,7 @@ def main(argv):
             temps[team] = round(temps[team] / (games_played[team] - 1), 2)
         else:
             temps[team] = round(temps[team] / games_played[team], 2)
+    
     # Calculate AVG.HUM
     for team in humidity:
         # Don't include superbowl data in temp/humidity calculation
@@ -151,17 +144,21 @@ def main(argv):
             humidity[team] = round(humidity[team] / (games_played[team] - 1), 2)
         else:
             humidity[team] = round(humidity[team] / games_played[team], 2)
+    
     # Calculate PER.WINS.HOME
     for team in per_wins_home:
         per_wins_home[team] = round((per_wins_home[team] / games_home[team])*100,2)
+    
     # Calculate PER.WINS.HOME
     for team in per_wins_away:
         per_wins_away[team] = round((per_wins_away[team] / games_away[team])*100,2)    
+    
     # Reset Iterator
     csv_raw_input.close() 
     csv_raw_input = open(argv[1], newline='')
     csv_file_input = csv.reader(csv_raw_input)
     numRow = 0
+    
     # Calculate PER.WINS.TEMP.LESS    
     for row in csv_file_input:
         if(numRow != 0):
@@ -230,22 +227,27 @@ def main(argv):
             numRow += 1
         else:
             numRow += 1
+    
     # Calculate PER.WINS.TEMP.LESS
     for team in per_wins_temp_less:
         per_wins_temp_less[team] = round( (per_wins_temp_less[team] / per_wins[team])*100 ,2)
+    
     # Calculate PER.WINS.TEMP.MORE
     for team in per_wins_temp_more:
         per_wins_temp_more[team] = round( (per_wins_temp_more[team] / per_wins[team])*100 ,2)
+    
     # Calculate PER.WINS.HUM.LESS
     for team in per_wins_hum_less:
         per_wins_hum_less[team] = round( (per_wins_hum_less[team] / per_wins[team])*100 ,2)
+    
     # Calculate PER.WINS.HUM.MORE
     for team in per_wins_hum_more:
         per_wins_hum_more[team] = round( (per_wins_hum_more[team] / per_wins[team])*100 ,2)
+    
     # Calculate PER.WINS
     for team in per_wins:
         per_wins[team] = round( (per_wins[team] / (games_home[team] + games_away[team]))*100 , 2)
-
+    
     # IF A TEAM DOES NOT EXIST IN A CATEGORY, ADD IT.
     for team in per_wins:
         if(team not in per_wins_home):
@@ -260,6 +262,7 @@ def main(argv):
             per_wins_hum_less[team] = 0.00
         if(team not in per_wins_hum_more):
             per_wins_hum_more[team] = 0.00
+    
     # Convert all percents to strings and append %
     for team in per_wins:
         per_wins[team] = str(per_wins[team]) + "%"
@@ -269,26 +272,18 @@ def main(argv):
         per_wins_temp_more[team] = str(per_wins_temp_more[team]) + "%"
         per_wins_hum_less[team] = str(per_wins_hum_less[team]) + "%"
         per_wins_hum_more[team] = str(per_wins_hum_more[team]) + "%"
-
-    # READY TO OUTPUT
-    # AVG.TEMP --> temps
-    # AVG.HUM --> humidity
-    # PER.WINS --> per_wins
-    # PER.WINS.HOME --> per_wins_home
-    # PER.WINS.AWAY --> per_wins_away
-    # PER.WINS.TEMP.LESS --> per_wins_temp_less
-    # PER.WINS.TEMP.MORE --> per_wins_temp_more
-    # PER.WINS.HUM.LESS --> per_wins_hum_less
-    # PER.WINS.HUM.MORE --> per_wins_hum_more
-
+   
     # Write all results
     csv_file_output.writerow(output_headers)
     numWritten = 0
-
+    
+    # No team/city specified, write all results to output
     if(len(argv) == 3):
         for team in per_wins:
             row = [team, addDecimals(temps[team]), addDecimals(humidity[team]), per_wins[team], per_wins_home[team], per_wins_away[team], per_wins_temp_less[team], per_wins_temp_more[team], per_wins_hum_less[team], per_wins_hum_more[team]]
             csv_file_output.writerow(row)
+    
+    # Team/city specified, write only appropirate results to output
     elif(len(argv) == 4):
         for team in per_wins:
             check = team.split(" ")
@@ -305,47 +300,10 @@ def main(argv):
         if(numWritten == 0):
             print("ERROR: No records for that team/city were found. Output will be blank.") 
             print("Please try again with different input.")
-
+    
     # Close files at end
     csv_raw_input.close() 
     csv_raw_output.close()  
 
-
-
-    
-
-
-
-
-
-
-
 if __name__ == "__main__":
     main(sys.argv)
-
-
-
-
-
-
-
-
-
-
-
-    # Don't know why, but the for loop below
-    # would not work as written unless I closed
-    # the CSV file and reopened it...
-    #
-    #csv_raw = open("hours.csv", "r")
-   #csv_file = csv.reader(csv_raw)
-
-   # print("\nPrint individual cells of CSV object.")
-    #for row in csv_file:
-
-
-        #for i in range(0, len(row)):
-        #    print(row[i])
-            
-   # print("Close CSV file.\n")
-   # csv_raw.close()
