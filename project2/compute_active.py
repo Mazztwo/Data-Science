67fb4995118dbf54d0894777824c4e65c44c3c2b
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-02-16 16:52:18 -0500

    Reads input edges and creates graph.

--- project2/compute_active.py
+++ project2/compute_active.py
@@ -3,6 +3,11 @@
 
 import csv
 import sys
+import networkx as nx
+
+# REMOVE BEFORE TURNING IN!!!
+import matplotlib.pyplot as plt
+###############################
 
 
 def main(argv):
@@ -14,18 +19,31 @@ def main(argv):
     # Open CSV input file in read mode
     # Open CSV output file in write mode, overwrite if already exists
     csv_raw_input = open(argv[1], newline='')
-    csv_raw_output = open(argv[3], "w")
+    #csv_raw_output = open(argv[3], "w")
 
 
     # Create CSV objects with raw files
     csv_file_input = csv.reader(csv_raw_input)
-    csv_file_output = csv.writer(csv_raw_output)
+    #csv_file_output = csv.writer(csv_raw_output)
 
+    # Create our directed graph
+    graph = nx.DiGraph()
+
+    # Read csv and create graph
+    for row in csv_file_input:
+        graph.add_edge(row[0],row[1])
+    
+
+
+    # REMOVE BEFORE TURNING IN!!!
+    nx.draw(graph, with_labels=True)
+    plt.show()
+    ###############################
 
 
     # Close files at end
     csv_raw_input.close() 
-    csv_raw_output.close() 
+    #csv_raw_output.close() 
 
 
 if __name__ == "__main__":
