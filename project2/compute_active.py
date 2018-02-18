f562f18a98f1b4f26035868e6afc71cdc806f36e
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-02-17 21:37:09 -0500

    Counts node traversals. Now must check if they are correct.

--- project2/compute_active.py
+++ project2/compute_active.py
@@ -5,10 +5,6 @@ import csv
 import sys
 import networkx as nx
 
-# REMOVE BEFORE TURNING IN!!!
-import matplotlib.pyplot as plt
-###############################
-
 
 def main(argv):
 
@@ -19,24 +15,28 @@ def main(argv):
     # Open CSV input file in read mode
     # Open CSV output file in write mode, overwrite if already exists
     csv_raw_input = open(argv[1], newline='')
-    #csv_raw_output = open(argv[3], "w")
+    csv_raw_output = open(argv[3], "w")
 
 
     # Create CSV objects with raw files
     csv_file_input = csv.reader(csv_raw_input)
-    #csv_file_output = csv.writer(csv_raw_output)
+    csv_file_output = csv.writer(csv_raw_output)
 
     # Create our directed graph
-    graph = nx.DiGraph()
+    directedGraph = nx.DiGraph()
 
-    # Create list of nodes
+    # Create lists
     nodes = []
+    edges = []
+    traversals = []
+    num_activations = {}
+    px = int(argv[2])
+    visited = []
+
 
     # Read csv and create graph
     for row in csv_file_input:
 
-        # Add edge to graph
-        graph.add_edge(row[0],row[1])
         node1 = int(row[0])
         node2 = int(row[1])
 
@@ -45,25 +45,44 @@ def main(argv):
             nodes.append(node1)
         if(node2 not in nodes):
             nodes.append(node2)
+
+        edges.append((node1,node2))
     
     # sort list of nodes
     nodes = sorted(nodes)
 
-    # REMOVE BEFORE TURNING IN!!!
-    nx.draw(graph, with_labels=True)
-    plt.show()
-    ###############################
+    #traversal = list(nx.edge_dfs(nx.DiGraph(edges), 1))
+    #print(traversal)
 
-    #list(edge_dfs(graph, nodes))
+    for node in nodes:
+        traversals.append(list(nx.edge_dfs(nx.DiGraph(edges), node)))
+
+        # Initialize list of activations. 
+        # Each node is counted a minimum of once.
+        num_activations[node] = 1
+
+    # Curr node we are on
+    i = 0
+    for traversal in traversals:
+        if(len(traversal) != 0):
+            for edge in traversal:
+                if (edge[1] % px == 0 and edge[1] not in visited):
+                    visited.append(edge[1])
+                    num_activations[nodes[i]] += 1
+            # Increment node        
+            i += 1
+            visited = []
 
 
-    print(nodes)
-
-
+    for key in num_activations.keys():
+        print(key, ",", num_activations[key])
+ 
     # Close files at end
     csv_raw_input.close() 
-    #csv_raw_output.close() 
+    csv_raw_output.close() 
 
 
 if __name__ == "__main__":
+
+        #print(traversal)
     main(sys.argv)
