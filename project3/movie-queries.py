367fcbef8c17ffaf0002ed7b3edae5ed796b6fb0
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-03-21 12:03:30 -0400

    Added [Q1] query.

--- project3/movie-queries.py
+++ project3/movie-queries.py
@@ -6,13 +6,40 @@ from neo4j.v1 import GraphDatabase, basic_auth
 
 #connection without authentication
 driver = GraphDatabase.driver("bolt://localhost", encrypted=False)
-
 session = driver.session()
 transaction = session.begin_transaction()
 
-result = transaction.run("MATCH (people:Person) RETURN people.name LIMIT 10")
-for record in result:
-   print (record['people.name'])
-   
+
+
+
+
+#[Q1]
+##########################################
+result = transaction.run("""
+MATCH (d:Director)-[:DIRECTED]->(m:Movie)
+RETURN d.name, count(m)
+ORDER BY count(m) DESC
+LIMIT 20
+;""")
+##########################################
+
+#for record in result:
+#  print (record['people.name'])
+
+
+
+
+
+#[Q2]
+##########################################
+result = transaction.run("""
+
+;""")
+##########################################
+
+
+
+
+
 transaction.close()
 session.close()
