670ce1fc0af52736f02e87113fcf4fc7257e44fa
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-04-12 14:00:11 -0400

    Added command line parsing.

--- project5/myrex.py
+++ project5/myrex.py
@@ -6,22 +6,40 @@
 import sys
 
 
+
+
+
+
+def predict():
+    print("Predict!")
+    
+    # Parse command line args
+    trainingFile = argv[2]
+    k = argv[3]
+    algorithm = argv[4]
+    userID = argv[5]
+    movieID = argv[6]
+
+def evaluate():
+    print("Evaluate!")
+
+    # Parse command line args
+    trainingFile = argv[2]
+    k = argv[3]
+    algorithm = argv[4]
+    testingFile = argv[5]
+
+
 def main(argv):
 
-    command = argv[1]
 
 
-    if(command == "predict"):
-        print("Predict!")
-
-    elif(command == "evaluate"):
-        print("Evaluate!")
+    if(argv[1] == "predict"):
+        predict()
+    elif(argv[1] == "evaluate"):
+        evaluate()
     else:
-        print("ERROR: '%s' is not an accepted argument." %(command))
-
-
-
-
+        print("ERROR: '%s' is not an accepted argument." %(argv[1]))
 
 if __name__ == "__main__":
     main(sys.argv)
