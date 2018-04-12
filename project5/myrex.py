54ec288a18446a28ce09f7356ec3738fcc56f65b
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-04-12 13:53:44 -0400

    Started to add command line argument parsing.

--- project5/myrex.py
+++ project5/myrex.py
@@ -3,8 +3,25 @@
 #   Project 5             #
 ###########################
 
+import sys
+
+
+def main(argv):
+
+    command = argv[1]
+
+
+    if(command == "predict"):
+        print("Predict!")
+
+    elif(command == "evaluate"):
+        print("Evaluate!")
+    else:
+        print("ERROR: '%s' is not an accepted argument." %(command))
 
 
 
 
 
+if __name__ == "__main__":
+    main(sys.argv)
